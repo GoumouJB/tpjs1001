172f6ba2029a5e38aea11cfab7db4e87c73b67aa
GoumouJB <12jbgou@gmail.com>
2024-09-14 09:02:56 +0000

    AnimationEE

--- JS/script.js
+++ JS/script.js
@@ -14,20 +14,34 @@ box.addEventListener('click', () => {
         // Si elle a été déplacée, on la ramène à la position initiale
         gsap.to(box, { x: 0, duration: 1 });
         // Faire réapparaître le texte (fade in)
-        gsap.to(boxText, { opacity: 1, duration: 1 });
+        //gsap.to(boxText, { opacity: 1, duration: 1 });
     }
     moved = !moved;  // Inverser l'état du déplacement après chaque clic
 });
 
 // Événement 2 : Au double-clic, faire tourner la boîte de 360 degrés
 box.addEventListener('dblclick', () => {
-    gsap.to(box, { rotation: "+=360", duration: 1 });
+    box.innerText = 'Excellent! Vous avez cliqué deux fois';
+    box.style.border = '5px solid rgb(180, 186, 169)';
+    gsap.set(box, { rotation: 0 });
+    gsap.to(box, { rotation: 360, duration: 3 });
 });
 
-// Événement 3 : Au survol de la souris, changer la couleur de fond en orange
+// Événement 3 : Survoler la boîte, changer la couleur et agrandir
 box.addEventListener('mouseover', () => {
-    gsap.to(box, { backgroundColor: "orange", duration: 0.5 });
+    box.innerText = 'Position actuelle!';
+    box.style.backgroundColor = 'orange';
+    gsap.to(box, { scale: 1.2, duration: 2 });
 });
+// Événement 2 : Au double-clic, faire tourner la boîte de 360 degrés
+//box.addEventListener('dblclick', () => {
+//    gsap.to(box, { rotation: "+=360", duration: 1 });
+//});
+
+// Événement 3 : Au survol de la souris, changer la couleur de fond en orange
+//box.addEventListener('mouseover', () => {
+ //   gsap.to(box, { backgroundColor: "orange", duration: 0.5 });
+//});
 
 // Événement 4 : Quand la souris quitte la boîte, restaurer la couleur de fond originale
 box.addEventListener('mouseout', () => {
